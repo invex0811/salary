1cb1a43039ce784c2c4cca9b70dad013e1d08375
invex0811 <60279071+invex0811@users.noreply.github.com>
2021-05-14 00:18:34 +0300

    add dist

---
+++ dist/precache-manifest.26dfaabccb9da2685a35c1b917f1dd84.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25161a2db6d4884cb40c",
+    "url": "/salary/css/app.187ac24c.css"
+  },
+  {
+    "revision": "3422cbc255f0aa595e2b",
+    "url": "/salary/css/chunk-0f67ecf2.da59305f.css"
+  },
+  {
+    "revision": "5c880f86fd582d4289ff",
+    "url": "/salary/css/chunk-546099e1.f4a8c1ed.css"
+  },
+  {
+    "revision": "1ed313cb7a15fcaf46fb",
+    "url": "/salary/css/chunk-7915c3a2.64aa1177.css"
+  },
+  {
+    "revision": "201b5cd896aef6912c79d78bec30af9a",
+    "url": "/salary/index.html"
+  },
+  {
+    "revision": "25161a2db6d4884cb40c",
+    "url": "/salary/js/app.ca5036bc.js"
+  },
+  {
+    "revision": "3422cbc255f0aa595e2b",
+    "url": "/salary/js/chunk-0f67ecf2.6aef4816.js"
+  },
+  {
+    "revision": "9afdb41237cbea2927fa",
+    "url": "/salary/js/chunk-0fd68bf4.75edfc5d.js"
+  },
+  {
+    "revision": "5c880f86fd582d4289ff",
+    "url": "/salary/js/chunk-546099e1.eb28e80e.js"
+  },
+  {
+    "revision": "1ed313cb7a15fcaf46fb",
+    "url": "/salary/js/chunk-7915c3a2.6fb5795a.js"
+  },
+  {
+    "revision": "2e1dc9bf30139e11a348",
+    "url": "/salary/js/chunk-vendors.19008dfe.js"
+  },
+  {
+    "revision": "09391e2549febc81f3ac512733f8fc5e",
+    "url": "/salary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/salary/robots.txt"
+  }
+]);
